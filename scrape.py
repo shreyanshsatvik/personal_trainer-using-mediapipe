f32dabad07759683cb81fa3b6424c7478b3925bf
Aditya Singh <aditya1999nov@gmail.com>
2021-04-10 13:02:30 +0530

    feat: crunches added

---
+++ scrape.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+
+url="https://colorlib.com/preview/#yogafun"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive'}
+br=requests.get(url,headers=hdr)
+soup=BeautifulSoup(br.text,"html.parser")
+print(soup)
